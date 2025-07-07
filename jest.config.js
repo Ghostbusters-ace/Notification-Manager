f97574fa2d5abd1106ecef88da475a8a54ac489f
GhostBuster <judin.malivert@ynov.com>
2025-07-07 11:05:13 +0200

    chore: initial project setup with configuration files

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const {createDefaultPreset} = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+    testEnvironment: "node",
+    transform: {
+        ...tsJestTransformCfg,
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/**/*.ts',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 50,
+            functions: 50,
+            lines: 50,
+            statements: 50,
+        }
+    },
+    testMatch: ["<rootDir>/test/**/*.spec.ts"]
+};
